07d8684ffb5a33189c8c2b386673e97ea2a6ed3d
Yash Jagtap <jagtapyash2512@gmail.com>
2022-05-29 18:50:27 +0530

    wallet connect

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import { useAddress, useMetamask } from "@thirdweb-dev/react";
+
 const App = () => {
+  const address = useAddress();
+  const connectWithMetamask = useMetamask();
+  console.log("Address: ", address);
+
+  // This is the case where the user hasn't connected their wallet
+  // to your web app. Let them call connectWallet.
+  if(!address){
+    return (
+      <div className="landing">
+        <h1>Welcome to Yash DAO</h1>
+        <button onClick={connectWithMetamask} className="btn-hero">
+          Connect your wallet
+        </button>
+      </div>
+    );
+  }
+  // This is the case where we have the user's address
+  // which means they've connected their wallet to our site!
   return (
     <div className="landing">
-      <h1>Welcome to Yash DAO</h1>
+      <h1>Wallet connected</h1>
     </div>
   );
 };
