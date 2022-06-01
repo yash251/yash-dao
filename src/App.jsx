358a753966608a3b2bd5bde5f3da10d0d175bce4
Yash Jagtap <jagtapyash2512@gmail.com>
2022-06-01 14:37:58 +0530

    check for membership nft

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,39 @@
-import { useAddress, useMetamask } from "@thirdweb-dev/react";
+import { useAddress, useMetamask, useEditionDrop } from '@thirdweb-dev/react';
+import { useState, useEffect } from 'react';
 
 const App = () => {
   const address = useAddress();
   const connectWithMetamask = useMetamask();
   console.log("Address: ", address);
+
+  // Initialize our editionDrop contract
+  const editionDrop = useEditionDrop("0x85b154BaA8716DDF4890eE35313EcDFf0b5F7A92");
+  // State variable for us to know if user has our NFT.
+  const [hasClaimedNFT, setHasClaimedNFT] = useState(false);
+
+  useEffect(() => {
+    // If they don't have a connected wallet, exit!
+    if (!address) {
+      return;
+    }
+
+    const checkBalance = async () => {
+      try {
+        const balance = await editionDrop.balanceOf(address, 0); // check if the user has our NFT
+        if (balance.gt(0)) {
+          setHasClaimedNFT(true);
+          console.log("🌟 this user has a membership NFT!");
+        } else {
+          setHasClaimedNFT(false);
+          console.log("😭 this user doesn't have a membership NFT.");
+        }
+      } catch (error) {
+        setHasClaimedNFT(false);
+        console.error("Failed to get balance", error);
+      }
+    };
+    checkBalance();
+  }, [address, editionDrop]);
 
   // This is the case where the user hasn't connected their wallet
   // to your web app. Let them call connectWallet.
